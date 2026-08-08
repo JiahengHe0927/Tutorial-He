a0635a3912f9be4feff2bc878951be0390a1ece9
JiahengHe0927 <hejiaheng0927>
2026-08-08 21:53:08 +0800

    feat: ProblemSet0 Answer

--- Tutorial/ProblemSet0/ProblemSet0.py
+++ Tutorial/ProblemSet0/ProblemSet0.py
@@ -1,7 +1,16 @@
 # Comments
 
-# Question 1 
-# Answer 1:
+# Question 1 6+4*10
+# Answer 1:46
 
-# Question 2: 
-# Answer 2:
+# Question 2: (6+4)*10
+# Answer 2:100
+
+#Question 3:23.0 to the 5th power
+#Answer 3:1.8721712305548575
+
+#Question 4:Positive root of the following equation: a. 34*x^2 + 68*x - 510 
+#Answer 4:3.0
+
+#Question 5:.cos(3.4)**2+math.sin(3.4)**2 
+#Answer 5:1.0
